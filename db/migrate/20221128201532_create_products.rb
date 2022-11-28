ce331bca0f44a0f3ec9d268aa4362d683809d01b
bozzywozzy <abozynski@pm.me>
2022-11-28 14:17:26 -0600

    Created Product and Category models and migrated the db.

---
+++ db/migrate/20221128201532_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :details
+      t.string :price
+      t.string :on_sale
+      t.string :new
+
+      t.timestamps
+    end
+  end
+end
