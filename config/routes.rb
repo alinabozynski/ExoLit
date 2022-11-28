34509500caf459d3eee8193746d5b2a4ed35c92e
bozzywozzy <abozynski@pm.me>
2022-11-28 14:39:32 -0600

    Created a products controller with index and show action/views. Configured the views to show product(s).

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'products', to: 'products#index'
+  get 'products/:id', to: 'products#show', as: 'product'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
