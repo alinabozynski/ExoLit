0843306fc6cd66cf6c4c66fed84587d602ebc49e
bozzywozzy <abozynski@pm.me>
2022-11-28 19:14:16 -0600

    Further validation for the Product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
   validates :name, presence: true
-  validates :price, presence: true, unless: ->(product) { product.on_sale.present? }
-  validates :on_sale, presence: true, unless: ->(product) { product.price.present? }
+  validates :price, format: { with: /\$\d+\.\d{2}/,  message: "Acceptable format for 'price' and 'on_sale' values: $___.{2}" }, presence: true, unless: ->(product) { product.on_sale.present? }
+  validates :on_sale, format: { with: /\$\d+\.\d{2}/, message: "Acceptable format for 'price' and 'on_sale' values: $___.{2}" }, presence: true, unless: ->(product) { product.price.present? }
 end
