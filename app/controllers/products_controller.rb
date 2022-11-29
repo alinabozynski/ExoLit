7c9189a00a91061c15336bf1eda68c3e6fe8f3d8
bozzywozzy <abozynski@pm.me>
2022-11-28 23:41:25 -0600

    Created pages for each category of products. Each category has a link in the navbar, which is part of the header and footer partials that were just created. A _categorized partial was also created to display the products on the category pages (does not include the category in the listing since the page is dedicated to that category already). The routes.rb file was also configured to accept these pages.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,64 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def bestsellers
+    @all = Product.includes(:category).all
+    @products = @all.where(:category => { :name => 'Bestsellers' })
+  end
+
+  def cleansers
+    @all = Product.includes(:category).all
+    @products = @all.where(:category => { :name => 'Cleansers' })
+  end
+
+  def eye_care
+    @all = Product.includes(:category).all
+    @products = @all.where(:category => { :name => 'Eye Care' })
+  end
+
+  def face_mists
+    @all = Product.includes(:category).all
+    @products = @all.where(:category => { :name => 'Face Mists' })
+  end
+
+  def makeup_removers
+    @all = Product.includes(:category).all
+    @products = @all.where(:category => { :name => 'Makeup Removers' })
+  end
+
+  def masks
+    @all = Product.includes(:category).all
+    @products = @all.where(:category => { :name => 'Masks' })
+  end
+
+  def mini_sized
+    @all = Product.includes(:category).all
+    @products = @all.where(:category => { :name => 'Mini Size' })
+  end
+
+  def moisturizers
+    @all = Product.includes(:category).all
+    @products = @all.where(:category => { :name => 'Moisturizers' })
+  end
+
+  def sets
+    @all = Product.includes(:category).all
+    @products = @all.where(:category => { :name => 'Sets' })
+  end
+
+  def skin_tools
+    @all = Product.includes(:category).all
+    @products = @all.where(:category => { :name => 'Skin Tools' })
+  end
+
+  def sunscreen
+    @all = Product.includes(:category).all
+    @products = @all.where(:category => { :name => 'Sunscreen' })
+  end
+
+  def treatments_serums
+    @all = Product.includes(:category).all
+    @products = @all.where(:category => { :name => 'Treatments & Serums' })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  get 'products', to: 'products#index'
+  get '/', to: 'products#index'
   get 'products/:id', to: 'products#show', as: 'product'
+  get '/bestsellers', to: 'products#bestsellers'
+  get '/cleansers', to: 'products#cleansers'
+  get '/eye_care', to: 'products#eye_care'
+  get '/face_mists', to: 'products#face_mists'
+  get '/makeup_removers', to: 'products#makeup_removers'
+  get '/masks', to: 'products#masks'
+  get '/mini_sized', to: 'products#mini_sized'
+  get '/moisturizers', to: 'products#moisturizers'
+  get '/sets', to: 'products#sets'
+  get '/skin_tools', to: 'products#skin_tools'
+  get '/sunscreen', to: 'products#sunscreen'
+  get '/treatments_serums', to: 'products#treatments_serums'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
