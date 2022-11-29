3aa6132deb33c91d0663b025d144b025e2810405
bozzywozzy <abozynski@pm.me>
2022-11-28 20:21:55 -0600

    Went to the website that I scraped data from and found the product descriptions that I was missing and filled in my CSV file where the web scraper failed to grab the details. Then I updated the validations for the Product model so that the 'detail' field is mandatory.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   validates :name, presence: true
+  validates :details, presence: true
   validates :price, format: { with: /\$\d+\.\d{2}/,  message: "Acceptable format for 'price' and 'on_sale' values: $___.{2}" }, presence: true, unless: ->(product) { product.on_sale.present? }
   validates :on_sale, format: { with: /\$\d+\.\d{2}/, message: "Acceptable format for 'price' and 'on_sale' values: $___.{2}" }, presence: true, unless: ->(product) { product.price.present? }
 end
