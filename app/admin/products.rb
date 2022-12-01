37e955cf269121bde4a13c78dcead6ca38b37ac3
bozzywozzy <abozynski@pm.me>
2022-12-01 00:32:53 -0600

    Implemented ActiveStorage and configured ActiveAdmin to upload images for new or existing products. Altered the views so that the image is displayed with the appropriate product.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,18 +1,13 @@
 ActiveAdmin.register Product do
+  permit_params :name, :details, :price, :on_sale, :new, :category_id, :image
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :name, :details, :price, :on_sale, :new, :category_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :details, :price, :on_sale, :new, :category_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-
+  # Formtastic
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' button
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_one_attached :image
+
   belongs_to :category
   validates :name, :details, presence: true
   validates :price, format: { with: /\$\d+\.[0-9][0-9]/,  message: "Acceptable format for 'price' values: $___.{2}" }, presence: true
