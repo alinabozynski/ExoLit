4ea488453162f27af04ade7d8bf0dd7345093eac
bozzywozzy <abozynski@pm.me>
2022-12-01 06:25:13 -0600

    Attempting to implement the ability to delete the image from the Product edit from in ActiveAdmin.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,12 +1,21 @@
 ActiveAdmin.register Product do
-  permit_params :name, :details, :price, :on_sale, :new, :category_id, :image
+  permit_params :name, :details, :price, :on_sale, :new, :category_id, :image, :delete_image
 
   # Formtastic
   form do |f|
     f.semantic_errors # shows errors on :base
     f.inputs          # builds an input field for every attribute
-    f.inputs do
-      f.input :image, as: :file
+
+    if f.object.image.attached?
+      f.inputs do
+        f.input :image, as: :file, :hint => image_tag(f.object.image, size: "300x300")
+      end
+      f.label :delete_image
+      f.checkbox :delete_image
+    else
+      f.inputs do
+        f.input :image, as: :file
+      end
     end
     f.actions         # adds the 'Submit' and 'Cancel' button
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,14 @@
 class Product < ApplicationRecord
   has_one_attached :image
 
+  attr_accessor :delete_image
+
+after_commit :remove_image, on: [:create, :destroy], if: :delete_image
+
+private def remove_image
+  Product.image.purge
+end
+
   belongs_to :category
   validates :name, :details, presence: true
   validates :price, format: { with: /\$\d+\.[0-9][0-9]/,  message: "Acceptable format for 'price' values: $___.{2}" }, presence: true
