dd0dcc888435d180f0484eaef2a78e0fb5125af0
bozzywozzy <abozynski@pm.me>
2022-12-09 23:41:25 -0600

    Customers can now checkout with Stripe.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -44,7 +44,7 @@ class ChargesController < ApplicationController
 
   def create
     # Amount in cents
-    @amount = 500
+    amount = session[:amount]
 
     @customer = Stripe::Customer.create({
       email: params[:stripeEmail],
@@ -53,7 +53,7 @@ class ChargesController < ApplicationController
 
     @charge = Stripe::Charge.create({
       customer: @customer.id,
-      amount: @amount,
+      amount: amount,
       description: 'Rails Stripe customer',
       currency: 'usd',
     })
