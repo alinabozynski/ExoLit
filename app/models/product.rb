64c3680f5d1d0f192c08daf3c30db2182c779ddb
bozzywozzy <abozynski@pm.me>
2022-12-11 06:14:16 -0600

    Made some final changes, including adding an action in the Product model that adds a default image if one is not attached. I decided to do this because the iamge URLs that were scraped from the web were throwing 403 Forbidden errors, therefore no products have images until Admin attaches one from ActiveAdmin.

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,10 +11,20 @@ class Product < ApplicationRecord
   validates :name, :details, presence: true
   validates :price, format: { with: /\$\d+\.\d\d/,  message: "Acceptable format for 'price' values: $____.__" }, presence: true
   validate :price_control
+  after_create :add_default_image
 
   def price_control
     if price.partition('.').last.length != 2
       errors.add(:price, 'The price must have 2 digits after the decimal.')
+    end
+  end
+
+  def add_default_image
+    unless image.attached?
+      image.attach(
+        io: File.open(Rails.root.join('app', 'assets', 'images', 'no_image_available.jpg')),
+        filename: 'no_image_available.jpg', content_type: 'image/jpg'
+      )
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -131,4 +131,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_11_043303) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "orders", "products"
 end
