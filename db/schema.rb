8a25ccda9a428b8f8ef4405b3e33edf3c76c8922
bozzywozzy <abozynski@pm.me>
2022-12-10 21:26:12 -0600

    Created an Order model and migrated the db.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_202741) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_11_032552) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -84,6 +84,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_202741) do
     t.datetime "updated_at", null: false
     t.integer "province_id"
     t.index ["province_id"], name: "index_customers_on_province_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal "total"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
